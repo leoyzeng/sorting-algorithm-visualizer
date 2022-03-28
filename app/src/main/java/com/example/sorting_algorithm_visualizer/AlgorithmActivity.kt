89210ccb45bc5da04832fb39b2d6a14aceb63315
Leo1478 <leozeng1478@gmail.com>
2022-03-27 22:24:53 -0400

    initial commit created activities for main and algorithms page created ui for main created buttons and slider for algorithms

---
+++ app/src/main/java/com/example/sorting_algorithm_visualizer/AlgorithmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sorting_algorithm_visualizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlgorithmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_algorithm)
+    }
+}
---
+++ app/src/main/java/com/example/sorting_algorithm_visualizer/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sorting_algorithm_visualizer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        attachListener()
+
+    }
+
+    /**
+     * attach all listeners on main activity
+     */
+    private fun attachListener() {
+        val buttonStart:Button = findViewById(R.id.buttonStart) // start button
+        buttonStart.setOnClickListener { // on click
+            val intent = Intent(this, AlgorithmActivity::class.java)
+            startActivity(intent) // open algorithm activity
+        }
+    }
+}
